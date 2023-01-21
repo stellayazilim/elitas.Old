35380270d8dce876ab6355f4a8138d8ca541c16c
kerimCetinbas <a.kerim.cetinbas@yandex.com>
2023-01-21 07:09:28 +0300

    prisma integration

---
+++ prisma/migrations/20230121040425_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Settings" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Settings_pkey" PRIMARY KEY ("id")
+);
